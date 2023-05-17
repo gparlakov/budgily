d8e1ee35f83a4b555e53556cfaac282ce84b2974
Georgi Parlakov <gparlakov@gmail.com>
2023-05-17 05:55:59 +0300

    feat: install tailwind

---
+++ budgily/tailwind.config.js
@@ -0,0 +1,21 @@
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx,mdx}'),
+  ],
+  plugins: [require('daisyui')],
+  darkMode: 'class',
+  daisyui: {
+    themes: ['light', 'dark'],
+    base: true,
+  },
+  theme: {
+    // extend: {
+    //   backgroundImage: {
+    //     'optional-theme-bg': "url('/Button-bg.svg')",
+    //   },
+    // },
+  },
+};
