efd6f87d7947b771226802e7ef8bfeb076bb725f
Georgi Parlakov <gparlakov@gmail.com>
2023-08-04 07:32:49 +0300

    feat: conditional slots w/o ctx
    - make the slots that don't use context be conditional
    - the <Slot /> that renders a component that needs the ctx needs to be rendered immediately otherwise the useContext fails

--- budgily/src/components/wizard/wizard.v2.tsx
+++ budgily/src/components/wizard/wizard.v2.tsx
@@ -1,152 +1,157 @@
 import {
-    Slot,
-    component$,
-    useStore,
     $,
-    createContextId,
-    useContextProvider,
     QRL,
     QwikIntrinsicElements,
-    useContext
-  } from '@builder.io/qwik';
-  import { Button } from '@qwik-ui/tailwind';
+    Slot,
+    component$,
+    createContextId,
+    useContext,
+    useContextProvider,
+    useStore
+} from '@builder.io/qwik';
 
 
 export type WizardContext = {
     onPage$: QRL<(id: number) => void>;
     state: {
-      current: number
+        current: number
     },
     next$: QRL<() => void>,
     prev$: QRL<() => void>,
-  }
+}
 
-  export const WizardContextId = createContextId<WizardContext>('Wizard_context');
+export const WizardContextId = createContextId<WizardContext>('Wizard_context');
 
+interface WizardProps {
+    steps: number;
+    useCustomActions?: boolean;
+    onWizardContext?: { ctx?: WizardContext }
+}
 
-  export const WizardV2 = component$(({ steps: stepCount }: { steps: number }) => {
-    const steps = Array.from({ length: stepCount }).map((_,i) => i);
+export const WizardV2 = component$(({ steps: stepCount, onWizardContext, useCustomActions }: WizardProps) => {
+    const steps = Array.from({ length: stepCount }).map((_, i) => i);
     const w = useStore({
-      current: 0,
-      nextDisabled: false,
-      prevDisabled: true
+        current: 0,
+        nextDisabled: false,
+        prevDisabled: true
     });
 
     const wiz = {
-      state: w,
-      next$: $(() => {
-        const hasNext = w.current + 1 < stepCount;
-        if (hasNext) {
-          w.prevDisabled = false;
-          w.current += w.current + 1;
-        }
-      }),
-      prev$: $(() => {
-        const hasPrev = w.current - 1 >= 0;
-        if (hasPrev) {
-          w.current = w.current - 1;
-          w.nextDisabled = false;
-        }
-      }),
-      onPage$: $((id: number) => {
-        if (w.current === id) {
-          return;
-        }
+        state: w,
+        next$: $(() => {
+            const hasNext = w.current + 1 < stepCount;
+            if (hasNext) {
+                w.prevDisabled = false;
+                w.current += w.current + 1;
+            }
+        }),
+        prev$: $(() => {
+            const hasPrev = w.current - 1 >= 0;
+            if (hasPrev) {
+                w.current = w.current - 1;
+                w.nextDisabled = false;
+            }
+        }),
+        onPage$: $((id: number) => {
+            if (w.current === id) {
+                return;
+            }
 
-        w.current = id;
-        w.nextDisabled = id + 1 >= stepCount;
-        w.prevDisabled = id - 1 < 0;
-      })
+            w.current = id;
+            w.nextDisabled = id + 1 >= stepCount;
+            w.prevDisabled = id - 1 < 0;
+        })
     };
 
     useContextProvider(WizardContextId, wiz);
-
+    if (onWizardContext) {
+        onWizardContext.ctx = wiz;
+    }
     return (<>
-      <div class="hero min-h-screen w-full bg-base-200">
-        <div class="hero-content w-full">
-          <div class="w-full">
-            <div class="text-sm breadcrumbs">
-              {/* <ul>
-                {
-                  steps
-                    .filter((s) => s <= w.current)
-                    .map((s) => <Slot key={s} name={`crumb${s}`} />)
-                }
-              </ul> */}
-            </div>
-            <div class="text-center">
-              {
-                steps
-                  // .filter((i) => i === w.current)
-                  .map((i) => <Slot key={`title-${i}`} name={`title-${i}`} />)
-              }
-              <div class="card w-full shadow-2xl bg-base-100 items-center">
-                <div class="card-body">
+        <div class="hero min-h-screen w-full bg-base-200">
+            <div class="hero-content w-full">
+                <div class="w-full">
+                    <div class="text-sm breadcrumbs">
+                        <ul>
+                            {
+                                steps
+                                    .map((s) => <Slot key={s} name={`crumb${s}`} />)
+                            }
+                        </ul>
+                    </div>
+                    <div class="text-center">
+                        {
+                            steps
+                                .map((i) => <Slot key={`title-${i}`} name={`title-${i}`} />)
+                        }
+                        <div class="card w-full shadow-2xl bg-base-100 items-center">
+                            <div class="card-body">
 
-                  {
-                    steps
-                      .filter((i) => i === w.current)
-                      .map((i) => <Slot key={`step-${i}`} name={`step-${i}`} />)
-                  }
+                                {
+                                    steps
+                                        .map((i) => <Slot key={`step-${i}`} name={`step-${i}`} />)
+                                }
 
+                            </div>
+
+                            <div class="card-actions">
+                                <Slot name="custom-actions" />
+
+                                {!useCustomActions && <><button
+                                    class="btn-secondary"
+                                    onClick$={wiz.prev$}
+                                    disabled={w.prevDisabled}
+                                >Prev</button>
+
+                                    <button
+                                        class="btn-primary"
+                                        onClick$={wiz.next$}
+                                        disabled={w.nextDisabled}
+                                    >Next</button></>}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="card-actions">
-                  <Button
-                    class="btn-secondary"
-                    onClick$={wiz.prev$}
-                    disabled={w.prevDisabled}
-                  >Prev</Button>
-
-                  <Button
-                    class="btn-primary"
-                    onClick$={wiz.next$}
-                    disabled={w.nextDisabled}
-                  >Next</Button>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </>)
-  })
+})
 
 
-  export type WizardStepProps = {
-    id: number
-  }
-  export const WizardStep = component$(({ id, }: WizardStepProps) => {
-    return <>
-      {id}
-      <Slot />
-    </>
-  })
-
-  export type WizardTitleProps = {} & QwikIntrinsicElements['h1'];
-  export const WizardTitle = component$((props: WizardTitleProps) => {
+export type WizardStepProps = {
+    step: number
+}
+export const WizardStep = component$(({ step }: WizardStepProps) => {
     const ctx = useContext(WizardContextId)
     return <>
-      <h1 {...props}>
-        <Slot />
-      </h1>
-      <Slot name='sub-title' />
+        {ctx.state.current === step ? <Slot /> : ''}
     </>
-  })
+})
 
-  export type WizardCrumbProps = { step: number } & QwikIntrinsicElements['li'];
-  export const WizardCrumb = component$(({ step, ...props }: WizardCrumbProps) => {
+export type WizardTitleProps = { step: number } & QwikIntrinsicElements['h1'];
+export const WizardTitle = component$(({ step, ...props }: WizardTitleProps) => {
+    const ctx = useContext(WizardContextId)
+    return <>
+        {ctx.state.current === step ? <><h1 {...props}>
+            <Slot />
+        </h1>
+            <Slot name='sub-title' /> </> : ''}
+    </>
+})
+
+export type WizardCrumbProps = { step: number } & QwikIntrinsicElements['li'];
+export const WizardCrumb = component$(({ step, ...props }: WizardCrumbProps) => {
 
     const ctx = useContext(WizardContextId);
     return <>
-      <li {...props}
-        class={step === ctx.state.current ? 'active' : ''}
-      >
-        <a
-          onClick$={() => { ctx.onPage$(step) }}
+        {ctx.state.current >= step ? <li {...props}
+            class={step === ctx.state.current ? 'active' : ''}
         >
-          <Slot />
-        </a>
-      </li>
+            <a
+                onClick$={() => { ctx.onPage$(step) }}
+            >
+                <Slot />
+            </a>
+        </li> : ''}
     </>
-  })
+})
--- budgily/src/routes/wizard/v2/index.tsx
+++ budgily/src/routes/wizard/v2/index.tsx
@@ -1,21 +1,28 @@
-import { component$ } from '@builder.io/qwik';
-import { WizardV2, WizardTitle } from 'budgily/src/components/wizard/wizard.v2';
+import { component$, useSignal, $, useStore } from '@builder.io/qwik';
+import { WizardV2, WizardTitle, WizardCrumb, WizardStep, WizardContext } from 'budgily/src/components/wizard/wizard.v2';
 
 
 export default component$(() => {
-
+    const w = useStore<{ctx?: WizardContext}>({})
     return <>
-      <WizardV2
-        steps={3}
-      >
-        {/* <WizardCrumb q: slot="crumb0" step={0}> Crumb1</WizardCrumb> */}
-        <WizardTitle q: slot="title-0"> First title</WizardTitle>
-        {/* <WizardStep id={1} q: slot="step-0">First Step</WizardStep> */}
+        <WizardV2
+            steps={2}
+            onWizardContext={w}
+            useCustomActions={true}
+        >
+            <WizardCrumb q: slot="crumb0" step={0}> Crumb1</WizardCrumb>
+            <WizardTitle q: slot="title-0" step={0}> First title</WizardTitle>
+            <WizardStep q: slot="step-0" step={0} >First Step</WizardStep>
 
 
-        {/* <WizardCrumb q: slot="crumb1" step={1}> Crumb2</WizardCrumb> */}
-        <WizardTitle q: slot="title-1"> title2</WizardTitle>
-        {/* <WizardStep id={2} q: slot="step-1">Step 2</WizardStep> */}
-      </WizardV2>
+            <WizardCrumb q: slot="crumb1" step={1}> Crumb2</WizardCrumb>
+            <WizardTitle q: slot="title-1" step={1}> title2</WizardTitle>
+            <WizardStep q: slot="step-1" step={1} >Step 2</WizardStep>
+
+            <>
+                <button q: slot="custom-actions" onClick$={() => w.ctx?.prev$()}>prev</button>
+                <button q: slot="custom-actions" onClick$={() => w.ctx?.next$()}>next</button>
+            </>
+        </WizardV2>
     </>
-  });
+});
