f36ac6e4786d6972f918ad2dedde1e97d6196133
Georgi Parlakov <gparlakov@gmail.com>
2023-09-09 12:50:13 +0300

    feat: remove categorise from demo header

--- budgily/src/components/header/header.tsx
+++ budgily/src/components/header/header.tsx
@@ -26,8 +26,7 @@ export default component$(() => {
     <header>
       <div class="bg-base-100">
         <h3>Hello and welcome to Budgily DEMO.</h3>
-        <p> The app has 1000 demo bank movement records that you can visualize, categorize and see in grid form. </p>
-        <p>Categorize by clicking on one of the chart sections or selecting the grid (top right corner) and selecting one or more rows of movements.</p>
+        <p>The app has 1000 demo bank movement records that you can visualize, categorize and see in grid form.</p>
         <p>Want to sign up for full version: <a class="link" href="https://docs.google.com/forms/d/1dsxhIgV8Hs2xphy_AxOdDg12iY0qW4GfqMcWafiZ5GE" target="_blank"> Sign up (Google Form)</a></p>
         <Button onClick$={() => tour.value += 1}>Want a tour?</Button>
       </div>
