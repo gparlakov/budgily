f5542ae4e6800f510b20cf02350be7cce33554b4
Georgi Parlakov <gparlakov@gmail.com>
2023-09-22 14:44:00 +0300

    remove the time setting

--- budgily/src/analytics/index.ts
+++ budgily/src/analytics/index.ts
@@ -1,9 +1,9 @@
 type ArrayType<T> = T extends Array<infer R> ? R : never;
 type TagType = ArrayType<typeof window.dataLayer>
 
-window.dataLayer.push(function() {
-    console.log('---time set')
-})
+// window.dataLayer.push(function() {
+//     console.log('---time set')
+// })
 
 export function tag(tag: TagType) {
     window.dataLayer.push(tag);
