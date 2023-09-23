2d258600c0032b38a703e116d463a9d7e4bc5c1e
Georgi Parlakov <gparlakov@gmail.com>
2023-09-23 12:49:54 +0300

    feat: clean up gtag

--- budgily/src/analytics/index.ts
+++ budgily/src/analytics/index.ts
@@ -1,13 +1,6 @@
-type ArrayType<T> = T extends Array<infer R> ? R : never;
-// type TagType = ArrayType<typeof window.dataLayer>
 
-// window.dataLayer.push(function() {
-//     console.log('---time set')
-// })
-
-// declare global {
-//     export function gtag(...tag: unknown[]) : void;
-// }
-
-// eslint-disable-next-line prefer-rest-params, @typescript-eslint/no-unused-vars
-export function gtag(..._args: unknown[]){window.dataLayer.push(arguments);}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function gtag(..._args: unknown[]){
+    // eslint-disable-next-line prefer-rest-params
+    window.dataLayer.push(arguments);
+}
